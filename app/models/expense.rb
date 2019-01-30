fcf797519a5a676181d1f51ef4b1e16629a4623d
sbassmed <sebastiancamilomarin@gmail.com>
2019-01-30 18:20:02 -0500

    Agrega las asociaciones user , expenses

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -9,8 +9,10 @@
 #  amount      :decimal(, )
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#  user_id     :integer
 #
 
 class Expense < ActiveRecord::Base
   belongs_to :category
+  belongs_to :user
 end
--- test/models/expense_test.rb
+++ test/models/expense_test.rb
@@ -9,6 +9,7 @@
 #  amount      :decimal(, )
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#  user_id     :integer
 #
 
 require 'test_helper'
