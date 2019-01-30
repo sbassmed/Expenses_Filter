805b4e09c1d4a3c6b369e89c7a1cf478f8e275c5
sbassmed <sebastiancamilomarin@gmail.com>
2019-01-30 18:12:32 -0500

    Cree llave foranea user_id , corri migración

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: expenses
+#
+#  id          :integer          not null, primary key
+#  category_id :integer
+#  date        :date
+#  concept     :string
+#  amount      :decimal(, )
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Expense < ActiveRecord::Base
   belongs_to :category
 end
--- test/models/expense_test.rb
+++ test/models/expense_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: expenses
+#
+#  id          :integer          not null, primary key
+#  category_id :integer
+#  date        :date
+#  concept     :string
+#  amount      :decimal(, )
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class ExpenseTest < ActiveSupport::TestCase
